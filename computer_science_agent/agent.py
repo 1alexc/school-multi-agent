6d4c56378ab7feb8d6edbffbd58821c35a82595d
Alexander Carr <1alexander.carr@gmail.com>
2026-05-24 01:03:53 +0100

    tools imported to csci and geog agents. Need to be tested on adk web server root agent when rate limit quota resets

--- computer_science_agent/agent.py
+++ computer_science_agent/agent.py
@@ -1,8 +1,13 @@
 from google.adk.agents.llm_agent import Agent
+from computer_science_agent.tools import explain_algorithm, fetch_programming_question
 
 computer_science_agent = Agent(
     model='gemini-2.5-flash',
     name='computer_science_agent',
     description='A helpful assistant for computer science questions.',
     instruction='You are an expert computer science teacher. Answer user questions related to computer science, programming, and algorithms clearly and accurately.',
+    tools=[
+        explain_algorithm,
+        fetch_programming_question
+    ]
 )
--- geography_agent/agent.py
+++ geography_agent/agent.py
@@ -1,8 +1,13 @@
 from google.adk.agents.llm_agent import Agent
+from geography_agent.tools import get_country_info, get_geography_trivia
 
 geography_agent = Agent(
     model='gemini-2.5-flash',
     name='geography_agent',
     description='A helpful assistant for geography questions.',
     instruction='You are an expert geography teacher. Answer user questions related to geography, maps, cultures, and physical environments clearly and accurately.',
+    tools=[
+        get_country_info,
+        get_geography_trivia
+    ]
 )
